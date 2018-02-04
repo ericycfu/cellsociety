ed528ef02a81db5d23c5073688a888d466c90f57
Frank <yy145@duke.edu>
2018-02-03 22:37:37 -0500

    game of life, segregation added

--- src/cellsociety_team06/Cell.java
+++ src/cellsociety_team06/Cell.java
@@ -1,5 +1,29 @@
 package cellsociety_team06;
 
 public class Cell {
-
+	private int currentState;
+	private int futureState;
+	private String[] myPropertys;
+	
+	public Cell(String[] propertys, int initialState){
+		myPropertys = propertys;
+		currentState = initialState;
+	}
+	
+	public int showCurrentState(){
+		return currentState;
+	}
+	
+	public String showCurrentProperty(){
+		return myPropertys[currentState];
+	}
+	
+	public void update(){
+		currentState = futureState;
+	}
+	
+	public void setFutureState(int nextState){
+		futureState = nextState;
+	}
+	
 }
---
+++ src/cellsociety_team06/Grid_Segregation.java
@@ -0,0 +1,50 @@
+package cellsociety_team06;
+
+import java.util.ArrayList;
+
+public class Grid_Segregation extends Grid{
+	
+	public Grid_Segregation(int rownum, int colnum, double parameter, Calculator myCalculator) {
+		super(rownum, colnum, parameter, myCalculator);
+	}
+
+	public void updateCell(double prob, int centerCellRow, int centerCellCol){
+		if (prob==1){
+			int random = getRandomNumberInRange(0, myCellsUnoccupiedNextIteration.size()-1);
+			myCells[centerCellRow][centerCellCol].setFutureState(myCalculator.getState("Unoccupied"));
+			myCellsUnoccupiedNextIteration.get(random).setFutureState(myCells[centerCellRow][centerCellCol].showCurrentState());
+			myCellsUnoccupiedNextIteration.remove(random);
+			myCellsUnoccupiedNextIteration.add(myCells[centerCellRow][centerCellCol]);
+		}
+		else if (prob==0){
+			myCells[centerCellRow][centerCellCol].setFutureState(myCells[centerCellRow][centerCellCol].showCurrentState());
+		}
+	}
+	
+	// takes in the locations of each cell and puts them into myGrid
+	
+	public ArrayList<Cell> findAdjacentCells(int row, int col){
+		ArrayList<Cell> adjacentCells = new ArrayList<Cell>();
+		if (checkBoundary(row-1,col-1)) adjacentCells.add(getCell(row-1,col-1));
+		if (checkBoundary(row-1,col)) adjacentCells.add(getCell(row-1,col));
+		if (checkBoundary(row-1,col+1)) adjacentCells.add(getCell(row-1,col+1));
+		if (checkBoundary(row,col-1)) adjacentCells.add(getCell(row,col-1));
+		if (checkBoundary(row,col)) adjacentCells.add(getCell(row,col));
+		if (checkBoundary(row,col+1)) adjacentCells.add(getCell(row,col+1));
+		if (checkBoundary(row+1,col-1)) adjacentCells.add(getCell(row+1,col-1));
+		if (checkBoundary(row+1,col)) adjacentCells.add(getCell(row+1,col));
+		if (checkBoundary(row+1,col+1)) adjacentCells.add(getCell(row+1,col+1));
+		return adjacentCells;
+	}
+
+	@Override
+	protected void updateUnoccupiedCellArray() {
+		myCellsUnoccupiedNextIteration = new ArrayList<Cell>();
+		for (int i = 0; i < myRowNum; i++)
+			for (int j = 0; j < myColNum; j++){
+				if (myCells[i][j].showCurrentProperty().equals("Unoccupied")) 
+					myCellsUnoccupiedNextIteration.add(myCells[i][j]);
+			}
+	}
+	
+}
