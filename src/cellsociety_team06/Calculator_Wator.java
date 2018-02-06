1adc1b98c477d1ecc34c171c22f43c1ebcd1c019
Frank Yin <yifan.yin@duke.edu>
2018-02-06 00:47:56 -0500

    Update Calculator_Wator.java

--- src/cellsociety_team06/Calculator_Wator.java
+++ src/cellsociety_team06/Calculator_Wator.java
@@ -5,26 +5,26 @@ import java.util.ArrayList;
 public class Calculator_Wator extends Calculator{
  
  public Calculator_Wator(String[] propertys, double inputparameter) {
-  super(propertys);
-  parameter = inputparameter;
+	 super(propertys);
+	 myParameter = inputparameter;
   // TODO Auto-generated constructor stub
  }
  
  @Override
  public double calculation(ArrayList<Cell> relatedCells, Cell centerCell) {
-  if (centerCell.showCurrentProperty().equals("Fish")){
-   for (Cell c: relatedCells){
-    if (c.showCurrentProperty().equals("Unoccupied"))
-     return 1; 
-   }
-  }
-  else if (centerCell.showCurrentProperty().equals("Shark")){
-   for (Cell c: relatedCells){
-    if (c.showCurrentProperty().equals("Unoccupied") || c.showCurrentProperty().equals("Fish"))
-     return 1; 
-   }
-  }
-  return 0;
- }
- 
-}
+	 if (centerCell.showCurrentProperty().equals("Fish")){
+		 for (Cell c: relatedCells){
+		     if (c.showCurrentProperty().equals("Unoccupied"))
+		     return 1;
+		     }
+		 }
+	  else if (centerCell.showCurrentProperty().equals("Shark")){
+	   for (Cell c: relatedCells){
+	    if (c.showCurrentProperty().equals("Unoccupied") || c.showCurrentProperty().equals("Fish"))
+	     return 1; 
+	   }
+	  }
+	  return 0;
+	 }
+	 
+	}
