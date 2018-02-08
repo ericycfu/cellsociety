199c1ea2694513cdd5e320d59fa32b23fe610de5
Frank Yin <yifan.yin@duke.edu>
2018-02-08 15:58:43 -0500

    Upload New File

---
+++ src/Cell.java
@@ -0,0 +1,80 @@
+package cellsociety_team06;
+
+public class Cell {
+
+	private int currentState;
+	private int futureState;
+	private String[] myProperties;
+	private double myChronon;
+	private double myEnergy;
+	private double initialEnergy;
+	
+	public Cell(String[] properties, int initialState){
+		myProperties = properties;
+		currentState = initialState;
+		futureState = currentState;
+		myChronon = 0;
+	}
+	
+	public Cell(String[] properties, int initialState, double initialEnergyinput){
+		myProperties = properties;
+		currentState = initialState;
+		futureState = currentState; // initialize futureState to be the same with currentState
+		myChronon = 0;
+		myEnergy = initialEnergy;
+		initialEnergy = initialEnergyinput;
+	}
+	
+	public void resetEnergy(){
+		myEnergy = initialEnergy;
+	}
+	
+	public void setEnergy(double value){
+		myEnergy = value;
+	}
+	
+	public void changeEnergy(double value){
+		myEnergy = myEnergy + value;
+	}
+	
+	public double showEnergy(){
+		return myEnergy;
+	}
+	
+	public void resetChronon(){
+		myChronon = 0;
+	}
+	
+	public void updateChronon(double value){
+		myChronon = value;
+	}
+	
+	public double showChronon(){
+		return myChronon;
+	}
+	
+	public int showCurrentState(){
+		return currentState;
+	}
+	
+	public String showCurrentProperty(){
+		return myProperties[currentState];
+	}
+	
+	public int showFutureState(){
+		return futureState;
+	}
+	
+	public String showFutureProperty(){
+		return myProperties[futureState];
+	}
+	
+	public void update(){
+		currentState = futureState;
+	}
+	
+	public void setFutureState(int nextState){
+		futureState = nextState;
+	}
+	
+}
