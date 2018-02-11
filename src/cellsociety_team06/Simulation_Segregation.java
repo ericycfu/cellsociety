5a46692103f620ba65a16ba93f879b8f10400412
JING <jy201@duke.edu>
2018-02-10 19:01:22 -0500

    simulation classes

--- src/cellsociety_team06/Simulation_Segregation.java
+++ src/cellsociety_team06/Simulation_Segregation.java
@@ -1,84 +1,45 @@
 package cellsociety_team06;
+
+import org.xml.sax.SAXException;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import java.util.*;
 
-public abstract class Simulation{
-	
-	private int celllength = 10;
-	private int buttonwidth = 40;
-	private int buttonheight = 20;
-	
-	private String SIMUL;
-	private Stage myPrimaryStage;
-	private int width;
-	private int height;
-	private ArrayList<String> States = new ArrayList<String>();
-	private ArrayList<Float> Probabilities = new ArrayList<Float>();
-	private ArrayList<Float> CellParameters = new ArrayList<Float>();
-	
-	private int MILLISECOND_DELAY = 1000 / 60;
-	private double SECOND_DELAY = 1.0 / 60;
-	private Paint BACKGROUND = Color.GREY;
-	
-	private String TITLE = "CellSociety_team06";
-	
-	private String filename;
-	private Group root = new Group();
-	private Rectangle[][] thegrid;
-	
-	private Grid lifeGrid;
-	private Calculator lifecalc;
-	
-	private FileChooser.ExtensionFilter extFilter;
-	private String SimType;
-	private String SimTitle;
-	private String SimAuthors;
-	private XMLReader myReader;
-	
-	public static int timer = 0;
-	public static boolean pauser = false;
-	 
-	private Button PAUSE = new Button("Pause");
-	private Button STEP = new Button("Step");
-	private Button FINISH = new Button("Finish");
-	private Button EXIT = new Button("EXIT");
-	private Button SWITCH = new Button("Switch");
-	private TilePane tilePane = new TilePane();
-	private Button FASTER = new Button("Faster");
-	private Button SLOWER = new Button("Slower");
-	private TilePane speeder = new TilePane();
-	private Label timedisplay;
-	private FileChooser fileChooser;
-	private String[] properties ;
-	private Cell thiscell;
-	private Scene scene;
-	
-	private String cellType;
-	
-	
-	
-	
-	
-	
-	/*public void cellgenerator(int x, int y){
-		
-		Rectangle R = new Rectangle(x,y,celllength,celllength);
-		R.setFill(Color.PLUM);
-		root.getChildren().add(R);
-		
-	}*/
-	
-	
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.io.File;  
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.io.BufferedReader;  
+import java.io.BufferedWriter;  
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Simulation_Segregation extends Simulation{
+
 	public void mover(Grid myGrid, Calculator myCalc){
 		myGrid.iterate();
 		
@@ -142,11 +103,7 @@ public abstract class Simulation{
 		}
 		timer++;
 		timedisplay.setText("Frame passed: " + Integer.toString(timer));
-
+	
 	}
-	
-	
-	
-	
 	
 }
