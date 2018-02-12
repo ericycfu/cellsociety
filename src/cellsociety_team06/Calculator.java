2685d0531b16bd930e2a6913a52761534f510e97
Frank <yy145@duke.edu>
2018-02-11 20:32:39 -0500

    some javadocs

--- src/cellsociety_team06/Calculator.java
+++ src/cellsociety_team06/Calculator.java
@@ -15,7 +15,17 @@ import java.util.List;
  * @author Frank Yin
  */
 public abstract class Calculator {
+	
+	/**
+	 * An array of String that stores the all possible states of one cell in the simulation.
+	 * It is mainly used here to help the process of setting cell states easier. 
+	 */
 	protected String[] myProperties;
+	
+	/**
+	 * A double that stores the necessary parameter that determines the updating of each cell 
+	 * in the society. 
+	 */
 	protected double myParameter;
 	
 	/**
@@ -28,7 +38,8 @@ public abstract class Calculator {
 	}
 	
 	/**
-	 * 
+	 * The second constructor comes into play when some parameter is 
+	 * required for updating the grid
 	 * @param properties: the set of all properties in this simulation 
 	 * @param parameter: the parameter used for updating the grid 
 	 */
@@ -38,13 +49,19 @@ public abstract class Calculator {
 	}
 	
 	/**
-	 * 
-	 * @return: 
+	 * This getter function returns the parameter value to the grid. 
+	 * @return: returns myParameter as a double
 	 */
 	public double showParameter(){
 		return myParameter;
 	}
 	
+	/**
+	 * This is a helper method that makes the updating cell state easier 
+	 * by returning the state value when given its String form. 
+	 * @param property: current state in its String form 
+	 * @return: returns the corresponding current state index 
+	 */
 	protected int getState(String property){
 		for (int i = 0; i < myProperties.length; i++)
 			if (property.equals(myProperties[i]))
@@ -52,6 +69,16 @@ public abstract class Calculator {
 		return -1; // state not found (error)
 	}
 	
+	/**
+	 * This abstract method returns the probability of the centerCell's 
+	 * moving behavior based on its adjacent cells and rules. 
+	 * @param relatedCells: the List of cells that are related to the 
+	 * centerCell's moving decision
+	 * @param centerCell: the centerCell that this method checks for 
+	 * its future motion
+	 * @return: a double from zero to one, with higher value representing 
+	 * higher probability of the occurrance of motion 
+	 */
 	public abstract double calculation(List<Cell> relatedCells, Cell centerCell);
 
 }
